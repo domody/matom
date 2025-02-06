e275fa9d37f786e0a9769bb9bc284af026fe617c
Dom Ody <dom20.ody07@gmail.com>
2025-02-07 00:53:21 +0100

    updated sidebar to have working links, added prettier tailwind, added inbox route

--- src/app/app/inbox/page.tsx
+++ src/app/app/inbox/page.tsx
@@ -1,8 +1,8 @@
-export default function Dashboard() {
+export default function Docs() {
   return (
     <div className="flex w-[calc(100%-16rem)] flex-col">
       <div className="border-border-muted flex h-14 w-full items-center border-b px-4">
-        <h3 className="font-medium">Dashboard / Home</h3>
+        <h3 className="font-medium">Inbox</h3>
       </div>
       <div className="flex w-full flex-col p-4"></div>
     </div>
