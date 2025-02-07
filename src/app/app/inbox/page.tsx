fbb5cbd2666ddc711c64f2e293326f6c5761e9d2
SpoonFish <89307474+SpoonFish@users.noreply.github.com>
2025-02-07 18:09:07 +0100

    testing out inbox message styles

--- src/app/app/inbox/page.tsx
+++ src/app/app/inbox/page.tsx
@@ -1,9 +1,17 @@
+import { InboxMessage } from "@/app/components/information/InboxMessage"
+
 export default function Docs() {
   return (
-    <div className="flex w-[calc(100%-16rem)] flex-col">
-      <div className="border-border-muted flex h-14 w-full items-center border-b px-4">
+    <div className="flex w-[calc(100%-16rem)] bg-red flex-col">
+      <div className="border-border-muted bg-surface-1 flex min-h-14 w-full items-center border-b px-4">
         <h3 className="font-medium">Inbox</h3>
       </div>
+      <div className="h-full">
+      <InboxMessage></InboxMessage>
+      <InboxMessage></InboxMessage>
+      <InboxMessage></InboxMessage>
+      <InboxMessage></InboxMessage>
+      <InboxMessage></InboxMessage></div>
       <div className="flex w-full flex-col p-4"></div>
     </div>
   );
